96ee35f2b3f9165329ba53d452d876ff387d8e84
dydwo1127 <dydwo1127@gmail.com>
2018-01-23 21:20:00 +0900

    resourceMangement 스크립트 생성

---
+++ Assets/Scripts/ResourceManagemnet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResourceManagement
+{
+    [System.Serializable]
+    public class ResourceData
+    {
+        public float oxygen;
+        public float carbon;
+        public float food;
+        public float coal;
+        public float electricity;
+        public float hydrogen;
+        public float water;
+        public float water_soiled;
+    }
+
+    public class ResourceConvert
+    {
+
+    }
+
+    public class ResourceTransfer
+    {
+
+    }
+
+    public class ResourceSave
+    {
+
+    }
+}
