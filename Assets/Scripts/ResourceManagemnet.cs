a478df0de8c95d1ea656e913e0cb44f2f2e5d7e3
ParkMyungHoon <eldpswp88@gmail.com>
2018-02-09 18:35:04 +0900

    리소스 데이터 생성자 추가

--- Assets/Scripts/ResourceManagemnet.cs
+++ Assets/Scripts/ResourceManagemnet.cs
@@ -9,29 +9,36 @@ namespace ResourceManagement
         string name;
         Dictionary<string, float> requirements;
         Dictionary<string, float> outputs;
+
+        Recipe(string name, Dictionary<string, float> requirements, Dictionary<string, float> outputs)
+        { }
     }
-    
+
     class ResourceData
     {
-        Dictionary<string, float> resources = new Dictionary<string, float>()
+        Dictionary<string, float> resources;
+        public ResourceData(float O,float O2, float CO2, float C, float H,float H2O,float H2O_soiled,float E,float food)
         {
-            { "O" , 100f },
-            { "O2" , 100f },
-            { "CO2" , 100f },
-            { "C" , 100f },
-            { "H" , 100f },
-            { "H2O" , 100f },
-            { "H2O_soiled" , 100f },
-            { "E" , 100f },
-            { "Food" , 100f }
-        };
+            this.resources = new Dictionary<string, float>
+            {
+                  { "O" , O },
+                  { "O2" , O2 },
+                  { "CO2" , CO2 },
+                  { "C" , C },
+                  { "H" , H },
+                  { "H2O" , H2O},
+                  { "H2O_soiled" ,H2O_soiled },
+                  { "E" , E },
+                  { "Food" , food }
+            };
+        }
     }
 
     static class ResourceEngine
     {
         static List<Recipe> recipe = new List<Recipe>()
-        {
-
+        {   
+            
         };
 
         static List<Recipe> Filter(ResourceData data)
