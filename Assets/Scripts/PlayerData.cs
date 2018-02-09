e8dfbc77a424962bd7a929c2555d10d14656036f
dydwo1127 <dydwo1127@gmail.com>
2018-02-09 18:56:55 +0900

    생성자 수정

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ResourceManagement;
+
+namespace PlayerData
+{
+    class ShipData
+    {
+        double O2_total;
+        double CO2_total;
+
+        Dictionary<int, ResourceData> resources = new Dictionary<int, ResourceData>()
+        {
+            {1, new ResourceData(O:100f) },
+            {2, new ResourceData(100f,100f,100f,100f,100f,100f,100f,100f,100f) }
+        };
+    }
+}
--- Assets/Scripts/ResourceManagemnet.cs
+++ Assets/Scripts/ResourceManagemnet.cs
@@ -22,7 +22,9 @@ namespace ResourceManagement
     class ResourceData
     {
         Dictionary<string, float> resources;
-        public ResourceData(float O,float O2, float CO2, float C, float H,float H2O,float H2O_soiled,float E,float food)
+        public ResourceData(
+            float O = 0,float O2 = 0, float CO2 = 0, float C = 0, float H = 0,
+            float H2O = 0,float H2O_soiled = 0,float E = 0,float food = 0)
         {
             this.resources = new Dictionary<string, float>
             {
